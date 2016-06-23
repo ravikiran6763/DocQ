2e1e72e56e1c87d4122d58bb3dc67c4af824a12e
Amit Tantia <ravikiran6763@gmail.com>
2016-06-23 13:30:48 +0530

    invite contacts css modified

--- www/js/contactscontroller.js
+++ www/js/contactscontroller.js
@@ -1,34 +1,6 @@
-// DoctorQuickApp.controller('contactsCtrl', function($scope, $cordovaContacts, $rootScope){
-//   $scope.toggle = false;
-//   $rootScope.headerTxt="Invite Reviews";
-//   $rootScope.showBackBtn=true;
-//   $rootScope.checkedValue = false;
-//   $rootScope.showBadge=false;
-//   $rootScope.showDocStatus=false;
-//
-//        $scope.getContacts = function() {
-//          $scope.phoneContacts = [];
-//          function onSuccess(contacts) {
-//            for (var i = 0; i < contacts.length; i++) {
-//              var contact = contacts[i];
-//              $scope.phoneContacts.push(contact);
-//            }
-//          };
-//          function onError(contactError) {
-//            alert(contactError);
-//          };
-//          var options = {};
-//          options.multiple = true;
-//          $cordovaContacts.find(options).then(onSuccess, onError);
-//        };
-//      })
-
 DoctorQuickApp.controller('contactsCtrl', function($scope, $cordovaContacts,$state,invitereviews){
-
       $scope.phoneContacts = [];
       function onSuccess(contacts) {
-
-
           for (var i = 0; i < contacts.length; i++) {
           var contact = contacts[i];
           $scope.phoneContacts.push(contact);
@@ -42,70 +14,30 @@ DoctorQuickApp.controller('contactsCtrl', function($scope, $cordovaContacts,$sta
       options.multiple = true;
       options.hasPhoneNumber =true;
       $cordovaContacts.find(options).then(onSuccess, onError);
-
       $scope.selectedlist = "";
-
       $scope.phones = {};
-
-
-
-
-
       var uniquevalues = [];
-
       $scope.selectedcontacts = function(contactno)
       {
-
-
-
             if($scope.phones[contactno])
             {
-
                 console.log(contactno);
-
               $scope.selectedlist = contactno.split(' ').join('');
-
               if($scope.selectedlist.length > 10)
               {
-
-
-
                 $scope.selectedlist = $scope.selectedlist.substring(3);
-
               }
               else
               {
-
-
                   $scope.selectedlist = $scope.selectedlist;
-
               }
-
-
               invitereviews.invitereviewpatient($scope.selectedlist);
-
-
-
             $state.go('templates.inviteresult');
-
-
-
-
             }
             else
             {
-
-              //do noting
+            //do noting
               console.log('not selected');
-
-
             }
-
-
-
       }
-
-
-
-
 })
