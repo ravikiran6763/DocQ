1b83898498308662e9412c23b0023ad52e36c953
Veeresh <veereshkolkur@gmail.com>
2016-06-29 12:30:50 +0530

    css modification

--- www/js/controller/PatientProfileCtrl.js
+++ www/js/controller/PatientProfileCtrl.js
@@ -1,4 +1,4 @@
-DoctorQuickApp.controller('patientProfileCtrl', function($scope,$rootScope,$ionicConfig,$localStorage,$http,LoginService,patientProfileDetailsService,$cordovaCamera) {
+DoctorQuickApp.controller('patientProfileCtrl', function($scope,$rootScope,$ionicConfig,$localStorage,$http,LoginService,patientProfileDetailsService,$cordovaCamera,$cordovaFileTransfer) {
 
 	$rootScope.headerTxt="Patient Profile";
 	$rootScope.showBackBtn=true;
@@ -17,161 +17,111 @@ console.log('failure data', error);
 
 
 
-
-			 $scope.takePicture = function()
-			 {
-
-				 var options = {
-						 quality: 75,
-						 destinationType: Camera.DestinationType.DATA_URL,
-						 sourceType: Camera.PictureSourceType.CAMERA,
-						 allowEdit: true,
-						 encodingType: Camera.EncodingType.JPEG,
-						 targetWidth: 300,
-						 targetHeight: 300,
-						 popoverOptions: CameraPopoverOptions,
-						 saveToPhotoAlbum: false
-				 };
+						$scope.getPhoto = function()
+						{
 
 
+										var options = {
+												quality: 75,
+												destinationType: Camera.DestinationType.DATA_URL,
+												sourceType: Camera.PictureSourceType.CAMERA,
+												allowEdit: true,
+												encodingType: Camera.EncodingType.JPEG,
+												targetWidth: 300,
+												targetHeight: 300,
+												popoverOptions: CameraPopoverOptions,
+												saveToPhotoAlbum: false
+										};
 
-								$cordovaCamera.getPicture(options).then(function (imageData) {
+											$cordovaCamera.getPicture(options).then(function (imageData) {
 
-										//$scope.imgURI = "data:image/jpeg;base64," + imageData;
+													//$scope.imgURI = "data:image/jpeg;base64," + imageData;
 
-											 $scope.lastPhoto = imageData;
-
-
-										var   ft = new FileTransfer(),
-										 reader = new FileReader(),
-										 fileName,
-										 options = new FileUploadOptions();
-								 options.fileKey="ffile";
-							 fileName = imageData.substr(imageData.lastIndexOf('/')+1);
-								 options.fileName=fileName;
-								 options.mimeType="image/jpeg";
-								 var params = {};
-							 options.fileKey = "file";
-							 options.chunkedMode = false;
+													$scope.lastPhoto = dataURItoBlob("data:image/jpeg;base64,"+imageData);
 
 
-							 $http.get('http://dqimages.s3-website-us-west-2.amazonaws.com/dqimages', {params:{"fileName": fileName}}).then(
-				function(resp) {
+													$scope.creds = {
 
-						reader.onloadend = function(e) {
-							 var reader = new FileReader();
-							console.log(this.result);
+																					bucket: 'dqimages',
+																					access_key: 'AKIAJY37ZZXEDGRYEJVA',
+																					secret_key: '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'
 
-							 AWS.config.update({ accessKeyId: resp.data.awsKey, secretAccessKey: resp.data.secretAccessKey });
-							 AWS.config.region = 'us-east-1';
-							 var bucket = new AWS.S3({ params: { Bucket: resp.data.bucket } });
-							 var params = { Key: fileName, ContentType: "image/jpeg", Body: this.result, ServerSideEncryption: 'AES256' };
+																					}
 
-							 bucket.putObject(params, function(err, data) {
-								 if(err) {
-									 // There Was An Error With Your S3 Config
-									 alert(err.message);
-									 return false;
-								 }
-								 else {
-									 // Success!
-									 alert('Upload Done');
-								 }
-							 }); //End putObject
-						 } //end onloadend
-				 var blob = dataURItoBlob(imageURI);
-				 reader.readAsBinaryString(blob);
+															console.log($scope.creds.bucket);
 
 
+						var bucket = new AWS.S3({ params: { Bucket: $scope.creds.bucket } });
 
-				 }, function(err) {
-					 console.err(err);
-				 }, {
-					 quality: 75,
-					 targetWidth: 320,
-					 targetHeight: 320,
-					 saveToPhotoAlbum: false,
-						destinationType: 0
-				 }); //End HTTP.GET
-			 }); // End Get Picture
- } //end GetPhoto
- function dataURItoBlob(dataURI) {
-	 // convert base64/URLEncoded data component to raw binary data held in a string
-	 var byteString;
-	 if (dataURI.split(',')[0].indexOf('base64') >= 0)
-			 byteString = atob(dataURI.split(',')[1]);
-	 else
-			 byteString = unescape(dataURI.split(',')[1]);
-
-	 // separate out the mime component
-	 var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-	 // write the bytes of the string to a typed array
-	 var ia = new Uint8Array(byteString.length);
-	 for (var i = 0; i < byteString.length; i++) {
-			 ia[i] = byteString.charCodeAt(i);
-	 }
-
-	 return new Blob([ia], {type:mimeString});
+						AWS.config.update({ accessKeyId: $scope.creds.access_key, secretAccessKey: $scope.creds.secret_key });
+					  AWS.config.region = 'Oregon';
 
 
+						console.log(bucket);
 
-								}, function (err) {
-										// An error occured. Show a message to the user
-
-										console.log("An error occured while accessing the camera");
-
-								});
+							if($scope.lastPhoto)
+							{
 
 
+									var params = { Key: 'ffile', ContentType:"image/jpeg" , Body: $scope.lastPhoto, ServerSideEncryption: 'AES256' };
+
+									bucket.putObject(params, function(err, data) {
+											if(err) {
+												// There Was An Error With Your S3 Config
+												alert(err.message);
+												return false;
+												console.log('error');
+											}
+											else {
+												// Success!
+												alert('Upload Done');
+												console.log('upload done');
+											}
+										})
+
+									.on('httpUploadProgress',function(progress) {
+												// Log Progress Information
+												console.log(Math.round(progress.loaded / progress.total * 100) + '% done');
+											});
+								}
+								else {
+									// No File Selected
+									alert('No File Selected');
+									console.log('no file selected');
+								}
 
 
+							},
+			 				function (err) {
+													// An error occured. Show a message to the user
+
+													console.log("An error occured while accessing the camera");
+
+											});
 
 
+						}
+
+						function dataURItoBlob(dataURI)
+						{
+
+									// convert base64/URLEncoded data component to raw binary data held in a string
+									 var byteString = atob(dataURI.split(',')[1]);
+									 var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+									 var ab = new ArrayBuffer(byteString.length);
+									 var ia = new Uint8Array(ab);
+									 for (var i = 0; i < byteString.length; i++)
+									 {
+									    ia[i] = byteString.charCodeAt(i);
+									 }
+
+									 var bb = new Blob([ab], { "type": mimeString });
+									 console.log(bb);
+									 return bb;
 
 
-
-
-			 }
-
-
-
-
-
-			//  $scope.choosePicture = function()
-			//  {
-			//
-			//
-			// 	 			var options = {
-			// 						quality: 75,
-			// 						destinationType: Camera.DestinationType.DATA_URL,
-			// 						sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
-			// 						allowEdit: true,
-			// 						encodingType: Camera.EncodingType.JPEG,
-			// 						targetWidth: 300,
-			// 						targetHeight: 300,
-			// 						popoverOptions: CameraPopoverOptions,
-			// 						saveToPhotoAlbum: false
-			// 				};
-			//
-			// 						$cordovaCamera.getPicture(options).then(function (imageData) {
-			// 								$scope.imgURI = "data:image/jpeg;base64," + imageData;
-			// 						}, function (err) {
-			// 								// An error occured. Show a message to the user
-			// 								console.log("An error occured while accessing the Gallery");
-			// 						});
-			//
-			//
-			// }
-			//
-			// $scope.getimage = function()
-			// {
-			//
-			//
-			// 		alert('called');
-			//
-			//
-			// }
+						}
 
 
 });
