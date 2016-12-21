9f092d06871e192d238cb29f253882420aadb5fd
Amit Tantia <ravikiran6763@gmail.com>
2016-12-21 19:39:28 +0530

    toggle sliding done

--- www/js/controller/Doctor/doctorScreensCtrl.js
+++ www/js/controller/Doctor/doctorScreensCtrl.js
@@ -1,41 +1,74 @@
-DoctorQuickApp.controller('doctorScreensCtrl', function($scope, $rootScope,$localStorage, $ionicConfig, $state, $ionicLoading, $interval, $ionicPlatform, $ionicPopup,$localStorage,doctoronoffdetails) {
+DoctorQuickApp.controller('doctorScreensCtrl', function($scope, $rootScope,$localStorage, $ionicConfig, $state, $ionicSideMenuDelegate,$ionicLoading, $interval, $ionicPlatform, $ionicPopup,$localStorage,doctoronoffdetails,doctorServices) {
 
   	$rootScope.headerTxt="DoctorQuick";
 		$rootScope.showBackBtn=false;
 		$rootScope.showNotification=true;
 		$rootScope.showBadge=true;
 		$rootScope.showDocStatus=false;
-    // $scope.notificationValue=true;
-    $ionicLoading.show();
+
+    $scope.docAvailable=true;
+    $scope.docNotAvailable=false;
+
+      $ionicLoading.show();
 
     console.log($state.$current.name);
+    $scope.emailNotification = 'Subscribed';
+    console.log($scope.emailNotification);
 
 
+    $scope.Online = function (message) {
+          console.log(message);
+          $scope.docAvailable=true;
+          $scope.docNotAvailable=false;
+          console.log($scope.docAvailable);
+          console.log($scope.docNotAvailable);
 
+
+        };
+    $scope.Offline = function (message) {
+          console.log(message);
+          $scope.docAvailable=false;
+          $scope.docNotAvailable=true;
+
+        };
+
+        $scope.clickToOnline = function (message) {
+              console.log(message);
+              $scope.docAvailable=true;
+              $scope.docNotAvailable=false;
+              console.log($scope.docAvailable);
+              console.log($scope.docNotAvailable);
+
+
+            };
+        $scope.clickToOffline = function (message) {
+              console.log(message);
+              $scope.docAvailable=false;
+              $scope.docNotAvailable=true;
+
+            };
+$ionicSideMenuDelegate.canDragContent(false)
+///////////////////////////////////////////////
     $rootScope.changeNotification = function (e)
     {
-
       if(e)
       {
           $scope.notificationValue = true;
-
         }
         else{
           $scope.notificationValue = false;
-
         }
         console.log('check box changed', $scope.notificationValue);
-
     }
 
-  if($localStorage.onOff == 1){
+    if($localStorage.onOff == 1){
     $scope.checkedValue=true;
+    }
+
+  $rootScope.changeNotification = function (e)
+  {
+
   }
-
-$rootScope.changeNotification = function (e)
-{
-
-}
 
 	$rootScope.changeStatus = function (e)
 	{
@@ -119,6 +152,7 @@ $rootScope.changeNotification = function (e)
 
 
     $interval(callAtInterval, 5000);
+    //$interval(lookForPrescription, 1000);
 
    	function callAtInterval() {
       doctoronoffdetails.getdoctorrequest($localStorage.user).then(function(response){
@@ -130,4 +164,27 @@ $rootScope.changeNotification = function (e)
    		// console.log('callAtInterval');
    	}
 
+    function lookForPrescription() {
+      // $state.go('templates.notesForPatient');
+      doctorServices.lookForPrescription($localStorage.user).then(function(response){
+      $scope.res = response;
+      // console.log($scope.res);
+      }).catch(function(error){
+      console.log('failure data', error);
+      })
+   		console.log('lookForPrescription');
+   	}
+
 })
+
+// $scope.pushNotificationChange = function() {
+//     console.log('docStatus Change', $scope.docStatus.checked);
+//     if($scope.docStatus.checked == 'online'){
+//       $scope.docAvailable=true;
+//       $scope.docNotAvailable=true;
+//     }
+//     if($scope.docStatus.checked == 'offline'){
+//       $scope.docAvailable=false;
+//       $scope.docNotAvailable=true;
+//     }
+//   };
