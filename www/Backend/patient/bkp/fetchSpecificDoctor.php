cdd670ce22cf3a6fff28bafe13935ee1b8c2aff3
Amit Tantia <ravikiran6763@gmail.com>
2017-01-14 16:31:14 +0530

    patient views correction completed

---
+++ www/Backend/patient/bkp/fetchSpecificDoctor.php
@@ -0,0 +1,28 @@
+<?php
+		require 'headers.php';
+
+    header('Content-type: text/html; charset=utf-8');
+
+		$postdata = file_get_contents("php://input");
+		if (isset($postdata))
+		{
+
+				$loginphno = json_decode($postdata);
+        $doctorDetails = array();
+
+ 				$sql = "select doctorfname,doctorMname,doctorLname,doctorEmail,doctorPhone,doctorPwd,doctorDegrees,practicingSince,doctorAge,doctorSex,doctorCountry,doctorCity,doctorAddress1,doctorAddress2,doctorPincode,doctorLanguage1,doctorLanguage2,doctorBankName,doctorAccountNum,doctorBankIfsc,doctorFee,doctorSpecialityId,doctorMedFlag,doctorMedNum,onoff from doctorDetails,doctor_onoff where doctor_onoff.doctor_phno=doctorDetails.doctorPhone and doctorPhone='$loginphno'";
+				$retval = mysql_query( $sql, $dbhandle );
+        while($row = mysql_fetch_array($retval))
+        {
+          $doctorDetails[] = $row;
+        }
+
+        if(! $retval )
+        {
+          die('Could not get data: ' . mysql_error());
+        }
+         echo json_encode($doctorDetails);
+		}
+	 mysql_close($dbhandle);
+
+?>
