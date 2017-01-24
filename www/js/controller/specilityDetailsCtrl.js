d184f48633eb53a6d43831c45e867e4d41401a7b
Veeresh <veereshkolkur@gmail.com>
2017-01-24 11:39:53 +0530

    under testing

--- www/js/controller/specilityDetailsCtrl.js
+++ www/js/controller/specilityDetailsCtrl.js
@@ -15,20 +15,20 @@ DoctorQuickApp.controller('specilityDetailsCtrl', function($state, $rootScope, $
 
 $rootScope.specialId = $stateParams.specialId;
 
-  //var username = "greet+"+$localStorage.user;
+// TO CHECK NO OF DOCTORS ONLINE IN VSEE
 
-  var username = "greet+8792618138,ravikiran6763@gmail.com";
-  var password = "DQ_patient";
-
-  var success = function(message)
-  {
-    alert(message);
-  }
-
-  var failure = function()
-  {
-    alert("Error calling Hello Plugin");
-  }
+  // var username = "greet+8792618138,ravikiran6763@gmail.com";
+  // var password = "DQ_patient";
+  //
+  // var success = function(message)
+  // {
+  //   alert(message);
+  // }
+  //
+  // var failure = function()
+  // {
+  //   alert("Error calling Hello Plugin");
+  // }
 
   // hello.logininformation(username,password,success, failure);
 
