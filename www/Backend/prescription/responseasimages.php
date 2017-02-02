6a3d355d6f3335f66ba00561b84719599189f895
Amit Tantia <ravikiran6763@gmail.com>
2017-02-02 12:59:52 +0530

    favDoc

--- www/Backend/prescription/responseasimages.php
+++ www/Backend/prescription/responseasimages.php
@@ -6,6 +6,13 @@
  $postdata = file_get_contents("php://input"); // TO RECIEVE POST REQUEST FROM ANGULAR JS
 
 
+	//$png= imagecreatefrompng('dq_loginlogo.png');
+//list($newwidth, $newheight) = getimagesize('dq_loginlogo.png');
+
+//$count = imagecreatetruecolor($width, $height);
+//imagecopyresampled($count, $jpeg, 0, 0, 0, 0, $width, $height, $width, $height);
+//imagecopyresampled($count, $png, 30, 20, 0, 0, $newwidth, $newheight, $newwidth, $newheight);
+
 	if(isset($postdata))
 	{
 
@@ -125,33 +132,35 @@
    			 $red   = imagecolorallocate($my_img, 0,   255,   0);
   	     $black = imagecolorallocate($my_img, 0, 0, 0);
 
+				 $font = 'Ubuntu-B.ttf';
 				imagestring( $my_img, 20, 20, 20, "Dr.$doctor_fullname",$text_colour );
 				// imagestring( $my_img, 4, 800, 25, "Dr.$doctor_fullname", $text_colour );
 
 				//DOCTOR INFORMATION LIKE NAME,ADDRESS,CITY,PIN AND DATE
 
-				imagestring( $my_img, 40, 20, 40, "$doctor_degrees", $text_colour );
-				imagestring( $my_img, 60, 20, 60, "$doctor_fulladdress", $text_colour );
-				imagestring( $my_img, 80, 20, 80, "$doctor_citypin", $text_colour );
+				imagestring( $my_img, 40, 20, 40, "$doctor_degrees",$font, $text_colour );
+				imagestring( $my_img, 60, 20, 60, "MCI No:123456", $font,$text_colour );
+				imagestring( $my_img, 80, 20, 80, "$doctor_fulladdress",$font, $text_colour );
+				imagestring( $my_img, 100, 20, 100, "$doctor_citypin",$font, $text_colour );
 
 				imagerectangle($my_img, 0, 0, 920, 1080, $line_colour1);
-				$thickness = 18;
-				imageline( $my_img, 0, 115, 920, 115, $line_colour );
+				// $thickness = 18;
+				// imageline( $my_img, 0, 115, 920, 115, $line_colour );
 
 				$thickness = 18;
-				imageline( $my_img, 0, 115, 920, 115, $line_colour );
+				imageline( $my_img, 0, 165, 920, 165, $line_colour );
 				//PATIENT INFORMATION LIKE NAME,AGE AND SEX
 				imagerectangle($my_img, 0, 0, 920, 1080, $line_colour1);
-				imagestring( $my_img, 145, 20, 145, "Patient Name: $patient_fullname", $text_colour );
-				imagestring( $my_img, 175, 20, 175, "Age: $patient_age ", $text_colour );
-				imagestring( $my_img, 175, 250, 175, "Gender:$patient_sex", $text_colour );
+				imagestring( $my_img, 185, 20, 185, "Patient Name: $patient_fullname", $font, $text_colour );
+				imagestring( $my_img, 205, 20, 205, "Age: $patient_age ", $font, $text_colour );
+				imagestring( $my_img, 205, 250, 205, "Gender:$patient_sex",$font, $text_colour );
 				$date= date("Y/m/d") ;
-				imagestring( $my_img, 145, 750, 145, "Date:$date", $text_colour );
+				imagestring( $my_img, 185, 750, 185, "Date:$date",$font, $text_colour );
 
 
 				//DIAGNOSIS SPECIFIED BY DOCTOR
-				imagestring( $my_img, 50, 20, 220, "Diagnosis", $text_colour );
-				imageline( $my_img, 20, 245, 100, 245, $line_colour );
+				imagestring( $my_img, 90, 20, 260, "Diagnosis",$font, $text_colour );
+				imageline( $my_img, 20, 245, 100, 245, $font, $line_colour );
 
 				$diagnosis="$mentioneddiagnosis";
 				$diagnosisText=explode("\n",wordwrap($diagnosis,90,"\n"));
@@ -164,7 +173,7 @@
 
 				}
 
-				imagestring( $my_img, 50, 20, 475, "Tests Recomended", $text_colour );
+				imagestring( $my_img, 50, 20, 475, "Tests Recomended",$font, $text_colour );
 				imageline( $my_img, 20, 500, 170, 500, $line_colour );
 
 				$tests="$mentionedtests";
@@ -177,7 +186,7 @@
 				  $y=$y+18;
 
 				}
-				imagestring( $my_img, 60, 20, 700, "Medications", $text_colour );
+				imagestring( $my_img, 60, 20, 700, "Medications", $font, $text_colour );
 				imageline( $my_img, 20, 720, 120, 720, $line_colour );
 
 				$medication="$mentionedmedication";
@@ -191,15 +200,42 @@
 
 				}
 
-				imagestring( $my_img, 100, 60, 990, "-SD-", $text_colour );
-				imagestring( $my_img, 1020, 60, 1020, "Dr.$doctor_fullname",$text_colour );
-				imagestring( $my_img, 1050, 60, 1050, "$doctor_degrees", $text_colour );
+				imagestring( $my_img, 100, 60, 990, "-SD-",$font,$text_colour );
+				imagestring( $my_img, 1020, 60, 1020, "Dr.$doctor_fullname",$font,$text_colour );
+				imagestring( $my_img, 1050, 60, 1050, "$doctor_degrees",$font, $text_colour );
+
 
 
  				 header( "Content-type: image/jpeg" );
 				//GENERATE JPEG FORMAT IMAGE BASED ON CONSULTATION ID BY THE PATIENT
+
 				imagejpeg( $my_img,"DocQuik$preCount.jpeg");
-				imagedestroy( $my_img );
+
+
+
+				imagedestroy($my_img);
+
+				$png = imagecreatefrompng('dq_loginlogo.png');
+				$jpeg = imagecreatefromjpeg("DocQuik$preCount.jpeg");
+
+				list($width, $height) = getimagesize("DocQuik$preCount.jpeg");
+				list($newwidth, $newheight) = getimagesize('dq_loginlogo.png');
+
+
+				$out = imagecreatetruecolor($width, $height);
+				imagecopyresampled($out, $jpeg, 0, 0, 0, 0, $width, $height, $width, $height);
+				imagecopyresampled($out, $png, 680, 20, 0, 0, $newwidth, $newheight, $newwidth, $newheight);
+				imagejpeg($out, "DocQuik$preCount.jpeg");
+
+				$base64 = base64_encode($out);
+
+				imagedestroy( $out );
+
+
+
+
+
+
 
 			}
 		}
@@ -215,12 +251,12 @@
 
 mysql_close($dbhandle);
 
-$png= imagecreatefrompng('dq_loginlogo.png');
-list($newwidth, $newheight) = getimagesize('dq_loginlogo.png');
+//$png= imagecreatefrompng('dq_loginlogo.png');
+//list($newwidth, $newheight) = getimagesize('dq_loginlogo.png');
 
-$count = imagecreatetruecolor($width, $height);
-imagecopyresampled($count, $jpeg, 0, 0, 0, 0, $width, $height, $width, $height);
-imagecopyresampled($count, $png, 30, 20, 0, 0, $newwidth, $newheight, $newwidth, $newheight);
+//$count = imagecreatetruecolor($width, $height);
+//imagecopyresampled($count, $jpeg, 0, 0, 0, 0, $width, $height, $width, $height);
+//imagecopyresampled($count, $png, 30, 20, 0, 0, $newwidth, $newheight, $newwidth, $newheight);
 
 
 // $base64Prescription = base64_encode($imagedata);
--- www/js/controller/callAcceptedCtrl.js
+++ www/js/controller/callAcceptedCtrl.js
@@ -64,8 +64,9 @@ $scope.checkWalletBalance = function()
 				{
 					alert("Error calling Hello Plugin");
 				}
+				$state.go('app.patient_summary',{calledDoctor:$rootScope.accptdDoc});
 
-				hello.greet(uname,pw,persontocall,success, failure);
+				// hello.greet(uname,pw,persontocall,success, failure);
 }
 
 
