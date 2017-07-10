21519bd8e13abba47af3d0c187c4c73e7fe5031e
Amit Tantia <ravikiran6763@gmail.com>
2017-07-10 15:18:37 +0530

    androd ready for testing

--- www/js/controller/myconsultationsCtrl.js
+++ www/js/controller/myconsultationsCtrl.js
@@ -58,10 +58,6 @@ function checkNewMessages()
 						console.log('failure data', error);
 						});
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 584c727c7c2256027beeb14d498a4940606d6de2
 						if($localStorage.doctororpatient == 'patient'){
 							myConsultationService.myConsultedDoctors($localStorage.user).then(function(response){
 									$rootScope.ConsultedDoctor=response;//store the response array in doctor details
@@ -111,15 +107,6 @@ function checkNewMessages()
 										console.log('failure data', error);
 										});
 						}
-<<<<<<< HEAD
-
-
-
-
-
-
-=======
->>>>>>> 584c727c7c2256027beeb14d498a4940606d6de2
 						// for (var keyandroid in dataofandroid)
 						// {
 						// if (dataofandroid.hasOwnProperty(keyandroid))
