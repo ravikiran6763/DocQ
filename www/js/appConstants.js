6fc5b27edebca08f22e56206976054dd1615cd5d
Amit Tantia <ravikiran6763@gmail.com>
2017-09-11 13:17:29 +0530

    server shifted to mumbai region

--- www/js/appConstants.js
+++ www/js/appConstants.js
@@ -15,7 +15,7 @@ DoctorQuickApp.constant('BASE_URL', {
         // 'url' :'http://ec2-52-39-133-220.us-west-2.compute.amazonaws.com/'
         //Staging
         //'url' : 'http://ec2-54-187-148-143.us-west-2.compute.amazonaws.com/'
-        'url' : 'http://ec2-52-66-68-161.ap-south-1.compute.amazonaws.com'
+        'url' : 'http://ec2-52-66-68-161.ap-south-1.compute.amazonaws.com/'
 
 
 
